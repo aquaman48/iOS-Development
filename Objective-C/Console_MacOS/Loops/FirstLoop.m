b3dcd903d2b40f2a581677ba1c9a89ba983ade46
aquaman48 <63140071+aquaman48@users.noreply.github.com>
2021-07-14 17:09:27 -0700

    Create FirstLoop.m

---
+++ Objective-C/Console_MacOS/Loops/FirstLoop.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  FirstLoop
+//
+//  Created by user193450 on 7/5/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        for (int i = 10; i > 0; i--)
+        {
+            NSLog(@"Objective- C programming is fun! Iteration %i", i);
+        }
+    }
+    return 0;
+}
