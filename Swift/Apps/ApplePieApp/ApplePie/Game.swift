742d08af252325e48bcea72543c7f14e115e8f44
aquaman48 <63140071+aquaman48@users.noreply.github.com>
2021-07-30 19:26:52 -0700

    Apple Pie Project

---
+++ Swift/Apps/ApplePieApp/ApplePie/Game.swift
@@ -0,0 +1,34 @@
+//
+//  Game.swift
+//  ApplePie
+//
+//  Created by user193450 on 7/27/21.
+//
+
+import Foundation
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+    
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            }
+            else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+}
